42c3410ac3027c98e7478939a52195b89debb035
Will Vatcher <will.vatcher@gmail.com>
2018-11-19 22:49:45 -0800

    Initial Commit

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Connect to mlab
+// Make sure to replace db strongs with user info
+mongoose.connect('mongodb://will:will1234@ds113063.mlab.com:13063/gql-badger');
+mongoose.connection.once('open', () => {
+  console.log('Connected to DB');
+})
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+app.listen(4000, () => {
+  console.log('Now listening to requests on port 4000')
+})
