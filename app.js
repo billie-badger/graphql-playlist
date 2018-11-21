2c05d258fbbfc512852660f525d9e8cfa9f8acb2
Will Vatcher <will.vatcher@gmail.com>
2018-11-21 14:45:22 -0800

    Set up concurrent front end and backend run script

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const db = 'mongodb://will:will1234@ds113063.mlab.com:13063/gql-badger';
+
+// Allow cross-origin requrests
+app.use(cors());
+
+// Connect to mlab
+// Make sure to replace db strongs with user info
+mongoose.connect(db, { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('Connected to DB');
+})
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+app.listen(4000, () => {
+  console.log('Now listening to requests on port 4000')
+})
