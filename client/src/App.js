bae33be0cbe24454ed830c4b1806267cb6b3ac34
Will Vatcher <will.vatcher@gmail.com>
2018-11-20 22:23:41 -0800

    Adding Front End with React: Book List displayed

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+// Components
+import BookList from './components/BookList';
+
+// Apollo Client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Vatchbrary Reading List</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
